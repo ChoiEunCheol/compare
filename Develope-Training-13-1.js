4af57aee89e77e841529664b20ccac863b42c27c
최은철 <wndqhrgkdlel@gmail.com>
2023-11-07 11:47:16 +0900

    생성자 함수로 변경

--- Develope-Training-13-1.js
+++ Develope-Training-13-1.js
@@ -1,34 +1,37 @@
 const fs = require("fs");
 const path = require("path");
 
-const isTxtFile = (fileName) => {
-  let result = path.extname(fileName).toLowerCase() === ".txt";
-  return result;
-};
-//extname : 파일의 확장자 반환
-//toLowerCase : 소문자로 변환
+class FileComparator {
+  constructor(operator, operand) {
+    this.operator = operator;
+    this.operand = operand;
+  }
 
-const compareFile = (operator, operand) => {
-  try {
-    if (!isTxtFile(operator) || !isTxtFile(operand)) {
-      console.error("둘 다 txt파일이 아닙니다.");
+  isTxtFile(fileName) {
+    return path.extname(fileName).toLowerCase() === ".txt";
+  }
+
+  compare() {
+    try {
+      if (!this.isTxtFile(this.operator) || !this.isTxtFile(this.operand)) {
+        console.error("둘 다 txt파일이 아닙니다.");
+        return false;
+      }
+
+      const filePathOperator = path.join(__dirname, operator);
+      const filePathOperand = path.join(__dirname, operand);
+
+      const dataOperator = fs.readFileSync(filePathOperator, "utf-8");
+      const dataOperand = fs.readFileSync(filePathOperand, "utf-8");
+
+      return dataOperator === dataOperand;
+    } catch (error) {
+      console.error(error);
       return false;
     }
-
-    const filePathOperator = path.join(__dirname, operator);
-    const filePathOperand = path.join(__dirname, operand);
-
-    const dataOperator = fs.readFileSync(filePathOperator, "utf-8");
-    const dataOperand = fs.readFileSync(filePathOperand, "utf-8");
-
-    let isResult = dataOperator === dataOperand;
-
-    return isResult;
-  } catch (error) {
-    console.error(error);
-    return false;
   }
-};
+}
 
-const areFilesEqual = compareFile('operator.txt','operand.txt');
-console.log(areFilesEqual);
+const comparator = new FileComparator('operator.txt', 'operand.txt');
+const areFileEqual = comparator.compare();
+console.log(areFileEqual);

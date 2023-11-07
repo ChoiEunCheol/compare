48a9e350ef0a9bb207947c57764635e7dd11cbdd
최은철 <wndqhrgkdlel@gmail.com>
2023-11-07 11:32:05 +0900

    파일 내용을 비교하는 함수

--- Develope-Training-13-1.js
+++ Develope-Training-13-1.js
@@ -1,9 +1,31 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const isTxtFile = (fileName) => {
-    let result = path.extname(fileName).toLowerCase() === '.txt';
-    return result;
-}
+  let result = path.extname(fileName).toLowerCase() === ".txt";
+  return result;
+};
 //extname : 파일의 확장자 반환
 //toLowerCase : 소문자로 변환
+
+const compareFile = (operator, operand) => {
+  try {
+    if (!isTxtFile(operator) || !isTxtFile(operand)) {
+      console.error("둘 다 txt파일이 아닙니다.");
+      return false;
+    }
+
+    const filePathOperator = path.join(__dirname, operator);
+    const filePathOperand = path.join(__dirname, operand);
+
+    const dataOperator = fs.readFileSync(filePathOperator, "utf-8");
+    const dataOperand = fs.readFileSync(filePathOperand, "utf-8");
+
+    let isResult = dataOperator === dataOperand;
+
+    return isResult;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
